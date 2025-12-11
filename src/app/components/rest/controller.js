ded997d2987bfb26116fe813f1dd6ab523607aaf
Haris Khan <haris@onekhan.com>
2025-12-11 11:20:03 -0500

    Add query hints feature to REST client
    - Load hints from external JSON file (config/query-hints.json)
    - Section-based dropdown with scrollable menu
    - Purple-styled button with dropup positioning
    - Default bookstore example for generic use

--- src/app/components/rest/controller.js
+++ src/app/components/rest/controller.js
@@ -13,6 +13,35 @@ angular.module('cerebro').controller('RestController', ['$scope', '$http',
     $scope.path = '';
     $scope.options = [];
 
+    // Query hints loaded from external file
+    $scope.queryHintSections = [];
+    $scope.queryHintsLoaded = false;
+
+    // Load query hints from external JSON file
+    $http.get('config/query-hints.json').then(
+        function(response) {
+          if (response.data && response.data.sections) {
+            $scope.queryHintSections = response.data.sections;
+            $scope.queryHintsLoaded = true;
+          }
+        },
+        function(error) {
+          // Silently fail - hints button will be hidden if no file
+          $scope.queryHintsLoaded = false;
+        }
+    );
+
+    $scope.loadQueryHint = function(query) {
+      if (query) {
+        $scope.editor.setValue(JSON.stringify(query, null, 2));
+        $scope.method = 'POST';
+        if (!$scope.path) {
+          $scope.path = '_search';
+        }
+        AlertService.info('Query hint loaded');
+      }
+    };
+
     var localStorageKey = 'indices';
 
     var success = function(response) {
